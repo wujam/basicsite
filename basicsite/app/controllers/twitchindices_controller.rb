8f018bc4db9d73f4f34aebc0d378f16610c77d93
James Wu <wu.jame@husky.neu.edu>
2017-03-05 04:55:56 -0500

    working code

--- basicsite/app/controllers/twitchindices_controller.rb
+++ basicsite/app/controllers/twitchindices_controller.rb
@@ -12,7 +12,7 @@ class TwitchindicesController < ApplicationController
     @twitchindex = Twitchindex.new 
   end
   def show
-    @twitchindex = Twitchindex.find(params[:id])
+    @twitchindex = getUserInfo(Twitchindex.find(params[:id]).name)
   end
   def edit
   end
@@ -24,5 +24,11 @@ class TwitchindicesController < ApplicationController
     def twitchindex_params
       params.require(:twitchindex).permit(:name)
     end
+    def getUserInfo(streamer)
+      response = Unirest.get "https://api.twitch.tv/kraken/users?login=#{streamer}",
+                 	headers:{"Accept" => "application/vnd.twitchtv.v5+json", "Client-ID" => "77u9uoixaequar58v12yee12iyaod9"}
+
+      return "Response code: #{response.code}\n\nHeaders: #{response.headers}\n\nResponse Body: #{response.body}"
+  end
 
 end
